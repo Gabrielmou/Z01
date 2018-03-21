e0cac5d8d88120470ae83e058b4db0bc7a40a9a7
Gabriel Oliva <gabriel.oliva@hotmail.com>
2018-03-21 15:52:29 -0300

    Inversor coments

--- Projetos/D-UnidadeLogicaAritmetica/src/rtl/inversor16.vhd
+++ Projetos/D-UnidadeLogicaAritmetica/src/rtl/inversor16.vhd
@@ -13,13 +13,13 @@ entity inversor16 is
 end entity;
 
 architecture rtl of inversor16 is
-
+--Cria um novo sinal b de 16bits
 signal b : STD_LOGIC_VECTOR(15 downto 0);
 
 begin
-
+--Padroniza esse sinal se z=1
 b <= (others => '1') when z = '1' else (others => '0');
-
+--Inverte
 y <= b xor a;
 
 end architecture;
