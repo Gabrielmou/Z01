6a35f7a2260af52d92001bbd6cf79380c054339a
Gabriel Oliva <gabriel.oliva@hotmail.com>
2018-03-29 00:32:46 -0300

    update register64

--- Projetos/E-LogicaSequencial/src/rtl/Register64.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/Register64.vhd
@@ -4,6 +4,7 @@
 
 Library ieee; 
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
   
 entity Register64 is
 	port(
@@ -13,3 +14,36 @@ entity Register64 is
 		output: out STD_LOGIC_VECTOR(63 downto 0)
 	);
 end entity;
+
+architecture rtl of Register64 is
+--Declarando componentes a importar
+	component Register32 is
+		port(
+			clock:   in STD_LOGIC;
+			input:   in STD_LOGIC_VECTOR(31 downto 0);
+			load:    in STD_LOGIC;
+			output: out STD_LOGIC_VECTOR(31 downto 0)
+		);
+	end component;
+
+begin 
+
+
+-- Primeiros bits da esquerda
+	A2: Register32 port  map (
+		clock => clock, 
+		input => input(63 downto 32), 
+		load => load, 
+		output => output(63 downto 32)
+		);
+
+
+-- Ultimos bits da direita
+	A3: Register32 port  map (
+		clock => clock, 
+		input => input(31 downto 0), 
+		load => load, 
+		output => output(31 downto 0)
+		);
+
+end rtl;
