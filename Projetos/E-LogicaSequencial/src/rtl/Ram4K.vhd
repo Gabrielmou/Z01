10984957f01707244a28586adf601c288200d7d7
Gabriel Noal <gabriel.oliva@hotmail.com>
2018-04-02 11:35:20 -0300

    missing ":" added

--- Projetos/E-LogicaSequencial/src/rtl/Ram4K.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/Ram4K.vhd
@@ -28,31 +28,31 @@ architecture rtl of Ram4k is
 
 	component Mux8Way16 is --Importando Mux8Way16
 	port(
-		a   in  STD_LOGIC_VECTOR(15 downto 0);
-		b   in  STD_LOGIC_VECTOR(15 downto 0);
-		c   in  STD_LOGIC_VECTOR(15 downto 0);
-		d   in  STD_LOGIC_VECTOR(15 downto 0);
-		e   in  STD_LOGIC_VECTOR(15 downto 0);
-		f   in  STD_LOGIC_VECTOR(15 downto 0);
-		g   in  STD_LOGIC_VECTOR(15 downto 0);
-		h   in  STD_LOGIC_VECTOR(15 downto 0);
-		sel in  STD_LOGIC_VECTOR(2 downto 0);
-		q   out STD_LOGIC_VECTOR(15 downto 0)
+		a:   in  STD_LOGIC_VECTOR(15 downto 0);
+		b:   in  STD_LOGIC_VECTOR(15 downto 0);
+		c:   in  STD_LOGIC_VECTOR(15 downto 0);
+		d:   in  STD_LOGIC_VECTOR(15 downto 0);
+		e:   in  STD_LOGIC_VECTOR(15 downto 0);
+		f:   in  STD_LOGIC_VECTOR(15 downto 0);
+		g:   in  STD_LOGIC_VECTOR(15 downto 0);
+		h:   in  STD_LOGIC_VECTOR(15 downto 0);
+		sel: in  STD_LOGIC_VECTOR(2 downto 0);
+		q:   out STD_LOGIC_VECTOR(15 downto 0)
 	);
 	end component;
 
 	component DMux8Way is --Importando Dmux8Way
 	port(
-		a   in  STD_LOGIC;
-		sel in  STD_LOGIC_VECTOR(2 downto 0);
-		q0  out STD_LOGIC;
-		q1  out STD_LOGIC;
-		q2  out STD_LOGIC;
-		q3  out STD_LOGIC;
-		q4  out STD_LOGIC;
-		q5  out STD_LOGIC;
-		q6  out STD_LOGIC;
-		q7  out STD_LOGIC
+		a:   in  STD_LOGIC;
+		sel: in  STD_LOGIC_VECTOR(2 downto 0);
+		q0:  out STD_LOGIC;
+		q1:  out STD_LOGIC;
+		q2:  out STD_LOGIC;
+		q3:  out STD_LOGIC;
+		q4:  out STD_LOGIC;
+		q5:  out STD_LOGIC;
+		q6:  out STD_LOGIC;
+		q7:  out STD_LOGIC
 	);
 	end component;
 
