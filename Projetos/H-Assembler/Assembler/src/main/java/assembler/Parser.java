e570c5540d884fe56f8ea7e4fc1daf4e4cedd53d
Gabrielmou <gabrielmoura98@gmail.com>
2018-05-06 22:39:56 -0300

    Parser Feito

--- Projetos/H-Assembler/Assembler/src/main/java/assembler/Parser.java
+++ Projetos/H-Assembler/Assembler/src/main/java/assembler/Parser.java
@@ -5,12 +5,21 @@
 
 package assembler;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Encapsula o código de leitura. Carrega as instruções na linguagem assembly,
  * analisa, e oferece acesso as partes da instrução  (campos e símbolos).
  * Além disso, remove todos os espaços em branco e comentários.
  */
 public class Parser {
+
+    BufferedReader reader = null;
+	String command;
 
     /** Enumerator para os tipos de comandos do Assembler. */
     public enum CommandType {
@@ -24,8 +33,23 @@ public class Parser {
      * @param file arquivo NASM que será feito o parser.
      */
     public Parser(String file) {
+    	
+    	try {
+    		FileReader arq = new FileReader(file);
+		    BufferedReader reader = new BufferedReader(arq);
+		}
 
-    }
+		// Trata o erro FileNotFoundException, se ele acontecer.
+		catch(FileNotFoundException exception) {
+			System.out.println(exception);
+		}
+
+		// Trata o erro IOException, se ele acontecer.
+        catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
     /**
      * Carrega uma instrução e avança seu apontador interno para o próxima
@@ -34,7 +58,20 @@ public class Parser {
      * @return Verdadeiro se ainda há instruções, Falso se as instruções terminaram.
      */
     public Boolean advance() {
-    	return null;
+        try {
+            String line;
+			line = reader.readLine();
+            if (line != null) {
+                return true;
+            }
+            else {
+            	return false;
+            }
+        }
+		catch(IOException exception) {
+			System.out.println(exception);
+		}
+        return false;
     }
 
     /**
@@ -42,7 +79,19 @@ public class Parser {
      * @return a instrução atual para ser analilisada
      */
     public String command() {
-    	return null;
+        try {
+			command = reader.readLine();
+			for(int x = 0; x < command.length();x++){
+    		    if (command.charAt(x) == ';'){
+    			    command = command.substring(0, x);
+                }
+			}
+		} 
+        catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+        }
+    	return command;
     }
 
     /**
@@ -54,7 +103,15 @@ public class Parser {
      * @return o tipo da instrução.
      */
     public CommandType commandType(String command) {
-    	return null;
+        if (command.substring(0,3) == "leaw"){
+    		return CommandType.A_COMMAND;
+        }
+        else if (command.charAt(command.length() - 1) == ':'){
+            return CommandType.L_COMMAND;
+        }
+        else{
+            return CommandType.C_COMMAND;
+        }
     }
 
     /**
@@ -64,7 +121,19 @@ public class Parser {
      * @return somente o símbolo ou o valor número da instrução.
      */
     public String symbol(String command) {
-    	return null;
+        int marcador = 0;
+        String symbol = "";
+        if (commandType(command) == CommandType.A_COMMAND){
+             for(int x = 0; x < command.length();x++){
+                if (command.charAt(x) == '$'){
+                    marcador = x;
+                }
+    		    else if (command.charAt(x) == ','){
+    			    symbol = command.substring(marcador, x);
+                }
+            }
+        }        
+    	return symbol;
     }
 
     /**
@@ -74,7 +143,11 @@ public class Parser {
      * @return o símbolo da instrução (sem os dois pontos).
      */
     public String label(String command) {
-    	return null;
+        String label = "";
+        if (commandType(command) == CommandType.L_COMMAND){
+            label = command.substring(0,command.length()-2);
+        }
+    	return label;
     }
 
     /**
@@ -83,8 +156,12 @@ public class Parser {
      * @param  command instrução a ser analisada.
      * @return um vetor de string contento os tokens da instrução (as partes do comando).
      */
-    public String[] instruction(String command) {
-    	return null;
-    }
-
+   public String[] instruction(String command) {
+        if (commandType(command) == CommandType.C_COMMAND){
+            String[] instruction = new String[3];
+            command = command.replace(',',' ');
+            instruction = command.split(" ");
+        }
+        return instruction;
+   }
 }
