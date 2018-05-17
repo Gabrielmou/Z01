7cde9ae3962962915197ab5dff224ae3326d51df
Gabriel Oliva <gabriel.oliva@hotmail.com>
2018-05-17 19:11:02 -0300

    Parser done

--- Projetos/J-VMTranslator/VMtranslator/src/main/java/vmtranslator/Parser.java
+++ Projetos/J-VMTranslator/VMtranslator/src/main/java/vmtranslator/Parser.java
@@ -11,6 +11,8 @@ package vmtranslator;
 
 import java.io.*;
 
+import javax.swing.JOptionPane;
+
 /**
  * Encapsula o código de leitura. Carrega as instruções na linguagem de máquina virtual a pilha,
  * analisa, e oferece acesso aos comandos.
@@ -81,10 +83,7 @@ public class Parser {
     public CommandType commandType(String command) {
     	String line[] = command.split(" ");
     	command = line[0];
-    	if (command.startsWith("arithmetic")) {
-			return CommandType.C_ARITHMETIC;
-		}
-    	else if (command.startsWith("push")) {
+    	if (command.startsWith("push")) {
 			return CommandType.C_PUSH;
 		}
     	else if (command.startsWith("pop")) {
@@ -109,8 +108,10 @@ public class Parser {
 			return CommandType.C_CALL;
 		}
     	else {
-    		return null;
+			return CommandType.C_ARITHMETIC;
 		}
+    	
+		
     }
 
 
@@ -122,7 +123,30 @@ public class Parser {
      * @return somente o símbolo ou o valor número da instrução.
      */
     public String arg1(String command) {
-		return command;
+    	String arg1 = null;
+    	try{
+    		
+        	CommandType type = commandType(command);
+        	if (type != CommandType.C_RETURN) {
+        		String line[] = command.split(" ");
+    			if (type == CommandType.C_ARITHMETIC) {
+    				arg1 = line[0];
+    				//System.out.println(arg1);
+//    				return arg1;
+    			}
+    			else {
+    				arg1 = line[1];
+    				//System.out.println(arg1);
+//    				return arg1;
+    			}
+    		}    		
+    	}
+    	catch (Exception e) {
+    		System.out.println(e);
+    		JOptionPane.showMessageDialog(null, e, "Error", JOptionPane.ERROR_MESSAGE);
+
+    	}
+		return arg1;
     }
 
     /**
@@ -132,7 +156,19 @@ public class Parser {
      * @return o símbolo da instrução (sem os dois pontos).
      */
     public Integer arg2(String command) {
-		return null;
+    	Integer arg2_int = null;
+    	try {
+        	CommandType type = commandType(command);
+			if (type == CommandType.C_PUSH || type == CommandType.C_POP || type == CommandType.C_FUNCTION || type == CommandType.C_CALL) {
+        		String line[] = command.split(" ");
+        		String arg2 = line[2];
+        		arg2_int = Integer.parseInt(arg2);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+    		JOptionPane.showMessageDialog(null, e, "Error", JOptionPane.ERROR_MESSAGE);
+		}
+		return arg2_int;
     }
 
     // fecha o arquivo de leitura
